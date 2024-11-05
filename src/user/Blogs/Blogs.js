a0aa7da5895b87d64f5712d38799f43cdbf15c4d
Adarsh Kumar <adarshku123456789@gmail.com>
2024-11-05 13:00:12 +0530

    Blogs

--- src/user/Blogs/Blogs.js
+++ src/user/Blogs/Blogs.js
@@ -1,7 +1,149 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import '../Blogs/Blogs.css';
+import axios from "axios";
 
 const Blogs = () => {
-    return <div>Blogs</div>;
+
+
+    const [Category, setCategory] = useState([]);
+    const [Blog, setBlog] = useState([]);
+    // const [BlogCategory, setBlogCategory] = useState([]);
+
+
+    useEffect(() => {
+        getCategory();
+        getBlog();
+        getBlogCategory();
+    }, [])
+    // category: req.params.category
+
+    const getCategory = () => {
+        axios.get('http://www.localhost:3001/category/')
+
+            .then(result => {
+
+                console.log(result.data);
+                setCategory(result.data.category);
+            })
+
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
+
+    const getBlog = () => {
+        axios.get('http://www.localhost:3001/blogs')
+
+            .then(result => {
+
+                console.log(result);
+                setBlog(result.data.blog);
+            })
+
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
+
+    const getBlogCategory = (category) => {
+        axios.get('http://www.localhost:3001/blogs/category/' + category)
+
+            .then(result => {
+
+                console.log(result.data);
+                setBlog(result.data.blog);
+            })
+
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
+
+
+
+    return (
+        <div className="main">
+            <div className="blogs">
+
+                <div className="blogs__blog">
+
+                    <div className="blogs__blog__data">
+                        {Blog.map(Data => (
+                            <div className="blogs__blog__data__blogItem">
+
+
+                                <img
+                                    className="blogs__blog__data__blogItem__img"
+                                    src={Data.imageUrl}
+                                    alt={Data.name} />
+
+
+                                <p
+                                    className="blogs__blog__data__blogItem__category">
+                                    {Data.category}
+
+                                </p>
+
+
+                                <p
+                                    className="blogs__blog__data__blogItem__title">
+                                    {Data.title}
+
+                                </p>
+
+                                <button
+                                    className="blogs__blog__btn">
+                                    Read More
+                                </button>
+
+
+                            </div>
+
+                        ))}
+
+                    </div>
+
+                </div>
+
+
+                <div className="blogs__category">
+
+                    <h1>All Category</h1>
+
+                    <button
+                        onClick={() => {
+                            getBlog()
+                        }}
+                        className="blogs__category__btn">
+                        All Blogs
+                    </button>
+
+                    {Category.map(data => (
+                        <div>
+
+                            <button
+                                onClick={() => {
+                                    getBlogCategory(data.name)
+                                }}
+                                className="blogs__category__btn">
+                                {data.name}
+                            </button>
+
+                        </div>
+
+                    ))}
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    );
 };
 
 export default Blogs;
--- src/user/Footer/Footer.js
+++ src/user/Footer/Footer.js
@@ -1,27 +1,102 @@
 import React from 'react';
 import '../Footer/Footer.css';
-import DeveloperImage from '../../assets/development.svg';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import TwitterIcon  from '@mui/icons-material/Twitter';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div className='footer'>
 
             <div className="footer__logo">
+
+
                 <img
-                    className="footer__logo__img"
-                    src={DeveloperImage}
-                    alt='developer' />
+                    className='footer__logo__img'
+                    src={require('../../assets/a.png')}
+                    alt='Adarsh' />
+
+
             </div>
 
             <div className="footer__nav">
-                <InstagramIcon />
-                <FacebookIcon />
-                <TwitterIcon />
-                <GitHubIcon />
+                <Link
+                    to='https://www.instagram.com/i_.a_k/'
+                    target="_blank"
+                    className='footer__nav__link'>
+                    <InstagramIcon />
+                    <span
+                        className='footer__nav__link__span'>
+                        Instagram
+                    </span>
+
+                </Link>
+
+                <Link
+                    to='https://www.facebook.com/I.A.Kr.GUPTA/'
+                    target='_blank'
+                    className='footer__nav__link'>
+                    <FacebookIcon />
+                    <span
+                        className='footer__nav__link__span'>
+                        Facebook
+                    </span>
+
+                </Link>
+
+                <Link
+                    to='https://x.com/i__a_k/'
+                    target='_blank'
+                    className='footer__nav__link'>
+                    <TwitterIcon />
+                    <span
+                        className='footer__nav__link__span'>
+                        Twitter
+                    </span>
+
+                </Link>
+
+                <Link
+                    to='https://github.com/adarsh0707-kumar/'
+                    target='_blank'
+                    className='footer__nav__link'>
+                    <GitHubIcon />
+                    <span
+                        className='footer__nav__link__span'>
+                        GitHub
+                    </span>
+
+                </Link>
+
+                <Link
+                    to='https://www.linkedin.com/in/adarsh-kumar-657315251'
+                    target='_blank'
+                    className='footer__nav__link'>
+                    <LinkedInIcon />
+                    <span
+                        className='footer__nav__link__span'>
+                        LinkedIn
+                    </span>
+                </Link>
+
+
+                <Link
+                    to='https://wa.me/917209798901'
+                    target='_blank'
+                    className='footer__nav__link'>
+                    <WhatsAppIcon />
+                    
+                    <span
+                        className='footer__nav__link__span'>
+                        WhatsApp
+                    </span>
+                </Link>
+
+
             </div>
 
         </div>
